330c20b94dfa613701e1e1aea867167a738b450c
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-01-23 13:16:22 -0800

    added current forecast and uv index

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+/* Psuedo Code
+1.) Add a search bar in HTML and add event listener on click to search weather API
+2.) Prior to this add DOM elements to place where weather information will go
+3.) Fetch code from weather API and have it display on main column
+4.) Grab following items from fetch weather conditions, temperature, humidity, windspeed, UV index
+5.) Assign icons to various data points UV index and 5 day forecast
+6.) Keep previous searches in city search history
+
+*/
+// DOM Elements
+$(document).ready(function(){
+    
+    // let showWeatherCard = document.querySelector("#search-button")
+    // let city = document.querySelector("#search-bar").value
+    
+    // showWeatherCard.addEventListener(click).siblings("#search-bar").val
+
+    $('#search-button').on('click', function(){
+        console.log('click')
+        const city = $('.search-bar').val()
+        searchWeather(city)
+        
+    })
+
+    function searchWeather(city){
+        $('#current-weather').empty()
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f38f6a7de25e9c5bfba8b768dc8d3f45&units=imperial`)
+        .then(response => response.json())
+        .then(response => {
+            
+            const card = $('<div>').addClass('card');
+            const cityName = $('<h3>').addClass('card-title').text(`${response.name}`)
+            const date = $('<h3>').addClass('card-title').text(new Date().toLocaleDateString())
+            const icon = $('<img>').attr('src', `http://openweathermap.org/img/w/${response.weather[0].icon}.png`);
+            const temp = $('<p>').addClass('card-text').text(`temperature: ${response.main.temp} °F`)
+            const humidity = $('<p>').addClass('card-text').text(`humidity: ${response.main.humidity} %`)
+            const wind = $('<p>').addClass('card-text').text(`wind-speed: ${response.wind.speed} mph`)
+            card.append(cityName, date, icon, temp, humidity, wind)
+            const lat = response.coord.lat
+            const lon = response.coord.lon
+
+            $('#current-weather').append(card)
+            getUV(lat, lon)
+        })
+
+
+
+    }
+    function getUV(lat, lon){
+        fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=78a87d050ef61463bd5907ddceff6458`)
+        .then(response => response.json())
+        .then(response =>{
+            console.log(response)
+            const uvIndex = $('<p>').addClass('card-text').text(`UV Index: ${response.value}`)
+            $('#current-weather .card').append(uvIndex)
+            
+        })
+    }
+
+    
+    
+
+
+
+})
+
